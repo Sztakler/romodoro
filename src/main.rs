0d287749452ac7193865ae39dc0ee48d4a86d569
metarth <krystian.jasionek@protonmail.com>
2026-01-25 12:47:03 +0100

    parse program args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "A simple Pomodoro timer in Rust")]
+struct Args {
+    // Number of sessions (pomodoros)
+    #[arg(short, long, default_value_t = 4)]
+    count: u32,
+
+    // Work time (in minutes)
+    #[arg(short, long, default_value_t = 25)]
+    work_time: u32,
+
+    // Break time (in minutes)
+    #[arg(short, long, default_value_t = 25)]
+    break_time: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Starting Pomodoro session: {} sessions.", args.count)
 }
