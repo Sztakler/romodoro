eb80f4e993495fdfc0b5064890695d0b2d2f155d
metarth <krystian.jasionek@protonmail.com>
2026-01-25 13:11:38 +0100

    implement run_timer method

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,56 @@
+use std::{
+    io::{self, Write},
+    thread,
+    time::Duration,
+};
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "A simple Pomodoro timer in Rust")]
 struct Args {
     // Number of sessions (pomodoros)
-    #[arg(short, long, default_value_t = 4)]
+    #[arg(short, long, default_value_t = 2)]
     count: u32,
 
     // Work time (in minutes)
-    #[arg(short, long, default_value_t = 25)]
+    #[arg(short, long, default_value_t = 1)]
     work_time: u32,
 
     // Break time (in minutes)
-    #[arg(short, long, default_value_t = 5)]
+    #[arg(short, long, default_value_t = 1)]
     break_time: u32,
+}
+
+fn run_timer(minutes: u32, message: &str) {
+    let total_seconds = minutes * 60;
+
+    for i in (1..=total_seconds).rev() {
+        let minutes = i / 60;
+        let seconds = i % 60;
+
+        print!("\r{} {:02}:{:02} remaining.", message, minutes, seconds);
+        io::stdout().flush().unwrap();
+        thread::sleep(Duration::from_millis(20));
+    }
+
+    print!("\r{} 00:00 remaining.\n", message);
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Starting Pomodoro session: {} sessions.", args.count);
+    println!("Starting Pomodoro cycle: {} sessions.", args.count);
 
     for i in 1..=args.count {
         println!("\n--- Session {}/{} ---", i, args.count);
 
+        run_timer(args.work_time, "🚀 Work:");
+
         if i < args.count {
-            println!("Break! Take a {} minutes rest.", args.break_time);
-        } else {
-            println!("Finished! Good job!");
+            run_timer(args.break_time, "☕ Break:");
         }
     }
+
+    println!("\n\r--- 🎉 Finished! Good job! 🎉 ---");
 }
